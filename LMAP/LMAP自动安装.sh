f91fb9d581f3e157a3c1386cf6801e9b3b4b4f8c
剑宇 <swso0313@outlook.com>
2019-11-28 16:54:24 +0800

    updata

--- LMAP/LMAP自动安装.sh
+++ LMAP/LMAP自动安装.sh
@@ -1,30 +1,40 @@
 #!/bin/bash
-echo "开始安装LMAP环境"
-echo "正在安装Mysql"
+sudo echo "开始安装LMAP环境"
+sudo echo "正在安装Mysql"
 sudo apt-get install -y mysql-server 
 sudo apt-get install -y mysql-client
 sudo apt-get install -y libmysqlclient-dev
-echo  "Mysql已安装"
+sudo echo  "Mysql已安装"
 
-echo "正在安装Apache"
+sudo echo "正在安装Apache"
 sudo apt-get install -y apache2
-echo  "Apache已安装 正在修改配置文件"
+sudo echo  "Apache已安装 正在修改配置文件"
 sleep	3
-sed -i's#/var/www/html#/home/web#g' /etc/apache2/sites-enabled/000-default.conf
-sed -i '169a <Directory /home/web>' /etc/apache2/apache2.conf
-sed -i '170a Options Indexes FollowSymLinks' /etc/apache2/apache2.conf            
-sed -i '171a AllowOverride None' /etc/apache2/apache2.conf
-sed -i '172a Require all granted' /etc/apache2/apache2.conf
-sed -i '173a </Directory>' /etc/apache2/apache2.conf
-echo "配置文件已修改"
+sudo sed -i 's#/var/www/html#/home/web#g' /etc/apache2/sites-enabled/000-default.conf
+sudo sed -i '169a <Directory /home/web>' /etc/apache2/apache2.conf
+sudo sed -i '170a Options Indexes FollowSymLinks' /etc/apache2/apache2.conf            
+sudo sed -i '171a AllowOverride None' /etc/apache2/apache2.conf
+sudo sed -i '172a Require all granted' /etc/apache2/apache2.conf
+sudo sed -i '173a </Directory>' /etc/apache2/apache2.conf
 
-echo  "正在安装PHP"
+sudo mkdir -p /home/web
+sudo touch /home/web/index.php
+sudo echo "<?php" > /home/web/index.php
+sudo echo "phpinfo();" >> /home/web/index.php
+sudo echo "?>" >> /home/web/index.php
+
+sudo echo "配置文件已修改"
+sudo echo  "正在安装PHP"
 sudo apt-get install -y php7.0
-echo "PHP已安装"
-echo "正在安装依赖文件"
+
+sudo echo "PHP已安装"
+sudo echo "正在安装依赖文件"
 sudo apt-get install -y libapache2-mod-php7.0
 sudo apt-get install -y php7.0-fpm
 sudo apt-get install php7.0-mysql
 sudo systemctl restart apache2.service 
 sudo systemctl restart mysql.service
-echo "LMAP已配置完毕"
+sudo echo "LMAP已配置完毕"
+ip=$(ifconfig |awk -F':' '{print $2}' |sed -n '2'p |sed 's#Bcast##g'
+)
+echo "请到${ip}查看"
